cc5279e192228739d139fe381b4893028ff6942a
Daewik <75206835+Daewik@users.noreply.github.com>
2021-05-23 16:58:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 var images=["father.jpg","mother.jpg","grandfather.jpg","grandmother.png","Me.jpg","Little brother.jpg"];    
-var names=['Family Book','Father','Mother','Grandfather','grandmother','Me','Little Brother'];
+var names=['Father','Mother','Grandfather','grandmother','Me','Little Brother'];
 
 var i=0;
 
 function Update()
 {
-  i++;
-  var number_of_family=6;
-  if(i>number_of_family)
+
+  
+  if(i==6)
   {
       i=0;
   }
-  var name= names[i];
-  var image= images[i];
-  document.getElementById('i1').src=image;
-  document.getElementById('name').innerHTML=names;
+  document.getElementById('i1').src=images[i];
+  document.getElementById('name').innerHTML=names[i];
+  i++;
 }
